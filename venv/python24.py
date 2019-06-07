e1776e869502026fbb18b758a4df42c551efc30e
Computer <ekpreetkaur2000@gmail.com>
2019-06-07 19:56:26 +0530

    commit

---
+++ venv/python24.py
@@ -0,0 +1,23 @@
+"""
+
+  strings
+  set
+  dictionary
+
+"""
+
+#johnsShop = 'johns coffee shop'
+#johnsShop = "john's coffee shop"
+#johnsShop = 'john\s coffee shop'
+johnsShop = r'john\'s coffee shop'      # raw string
+print(johnsShop)
+
+message = "this is awesome day." \
+           "we will" \
+           "have fun"
+print(message)
+
+quote = """this is awesome day
+we will have fun"""
+
+print(quote)
